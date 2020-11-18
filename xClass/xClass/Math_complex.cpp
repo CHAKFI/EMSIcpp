228953a8122587c2aaea8fb21bf38c0746a7fe29
CHAKFI <ahmedchakfi1999@gmail.com>
2020-11-18 23:31:21 +0100

    upd math_complex.cpp

--- xClass/xClass/Math_complex.cpp
+++ xClass/xClass/Math_complex.cpp
@@ -6,14 +6,25 @@ Math_complex::Math_complex(double r, double i)
 	cout << "Constructeur d'initialisation: \n";
 	this->re = r;
 	this->im = i;
-
-	
 }
+
+
 
 Math_complex::Math_complex(const Math_complex& obj)
 {
 	this->re = obj.re;
 	this->im = obj.im;
+}
+
+Math_complex Math_complex::module(const Math_complex& obj)
+{
+
+	return Math_complex();
+}
+
+Math_complex Math_complex::conjugue(const Math_complex& obj)
+{
+	return Math_complex();
 }
 
 void Math_complex::Display()
